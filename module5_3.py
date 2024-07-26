aff9cc305673b891c1baf8b6a4be1beae4334502
Arimarenko <rimarenko84@mail.ru>
2024-07-26 13:17:16 +0300

    Update module5_3.py

--- module5_3.py
+++ module5_3.py
@@ -1,64 +1,53 @@
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, floors):
         self.name = name
-        self.number_of_floors = number_of_floors
-
-    def go_to(self, new_floor):
-        if new_floor <= self.number_of_floors and new_floor >= 1:
-            for floor in range(1, new_floor + 1):
-                print(floor)
-        else:
-            print("Такого этажа не существует")
-
-    def __len__(self):
-        return self.number_of_floors
+        self.floors = floors
 
     def __str__(self):
-        return f'ЖК {self.name}, этажей: {self.number_of_floors}'
+        return f'Название: {self.name}, кол-во этажей: {self.floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.floors == other.floors
+        return NotImplemented
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.floors < other.floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.floors <= other.floors
+        return NotImplemented
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.floors > other.floors
+        return NotImplemented
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.floors >= other.floors
+        return NotImplemented
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.floors != other.floors
+        return NotImplemented
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            return House(self.name, self.floors + value)
+        return NotImplemented
 
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            return House(self.name, value + self.floors)
+        return NotImplemented
 
-
-h1 = House('Эльбрус', 10)
-h2 = House('Акация', 20)
-
-print(h1)
-print(h2)
-print(h1 == h2)  # __eq_
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-h1 += 10  # __iadd__
-print(h1)
-h2 = 10 + h2  # __radd__
-print(h2)
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.floors += value
+            return self
+        return NotImplemented
