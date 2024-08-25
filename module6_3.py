3bc3cd2ec1fc08ab5f5748c7796f02a8ff9e9e0d
Arimarenko <rimarenko84@mail.ru>
2024-08-25 19:43:02 +0300

    Update module6_3.py

--- module6_3.py
+++ module6_3.py
@@ -2,6 +2,7 @@ class Horse:
     def __init__(self):
         self.x_distance = 0
         self.sound = 'Frrr'
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
@@ -19,18 +20,20 @@ class Eagle:
 class Pegasus(Horse, Eagle):
     def __init__(self):
         super().__init__()
+        super().__init__()  # Вызываем конструкторы родителей по порядку наследования
 
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-        return self.x_distance, self.y_distance
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
         print(self.sound)
 
 
+# Пример использования:
 p1 = Pegasus()
 
 print(p1.get_pos())
